f93a321b0af4865b9c012fe9aec8d20fa1d663f2
Martin Ronquillo Vargas <89862467+mRonquillov@users.noreply.github.com>
2021-09-10 10:10:32 -0500

    Feature permissions assign admin permissions (#89)
    * corregido un permiso (borrar tipo documento)
    
    * permisos de administrador

--- routes/channels/type-document.php
+++ routes/channels/type-document.php
@@ -7,4 +7,4 @@ Route::get('/type-document', [TypeDocumentController::class, 'index'])->middlewa
 Route::get('/type-document/{id}', [TypeDocumentController::class, 'show'])->middleware(['auth:sanctum', 'permission:type-document-obtener-tipo-documento']);
 Route::post('/type-document', [TypeDocumentController::class, 'store'])->middleware(['auth:sanctum', 'permission:type-document-crear-tipo-documento']);
 Route::put('/type-document/{typeDocument}', [TypeDocumentController::class, 'update'])->middleware(['auth:sanctum', 'permission:type-document-actualizar-tipo-documento']);
-Route::delete('/type-document/{typeDocument}', [TypeDocumentController::class, 'destroy'])->middleware(['auth:sanctum', 'permission:type-document-borrar-tipo-tipo']);
+Route::delete('/type-document/{typeDocument}', [TypeDocumentController::class, 'destroy'])->middleware(['auth:sanctum', 'permission:type-document-borrar-tipo-documento']);
