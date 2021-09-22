ffdfc8d8b869e998459a5ee922b2e49e07203354
Rafael Marín Cobeña <89403271+rmarincobena@users.noreply.github.com>
2021-09-22 16:38:28 -0500

    Feature storage apidocument aws (#162)
    * add migration filesystems in landlord
    
    * services and api resources for storage file in different channels (aws, api-document of links).
    
    * generate documentation swagger
    
    * add seeder default (type_documents) by connection landlord
    
    * delete Psr17Factory
    
    * remove dependencies spatie/laravel-ray
    
    * remove authorization middleware

--- routes/channels/filesystems.php
+++ routes/channels/filesystems.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\FileSystemController;
 
-Route::get('/show', [FileSystemController::class, 'showFile'])->middleware(['auth:sanctum']);
-Route::get('/download', [FileSystemController::class, 'downloadFile'])->middleware(['auth:sanctum']);
+Route::get('/show', [FileSystemController::class, 'showFile'])->middleware([]);
+Route::get('/download', [FileSystemController::class, 'downloadFile'])->middleware([]);
 /* Route::post('/upload', [FileSystemController::class, 'uploadFile'])->middleware(['auth:sanctum']); */
