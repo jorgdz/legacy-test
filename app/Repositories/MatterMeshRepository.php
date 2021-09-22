30711f75be42a40460bf513a22e3f435bc1f3df1
etcruz <80907473+etcruz@users.noreply.github.com>
2021-09-22 14:56:11 -0500

    Feature mesh get all subjects class (#157)
    * Listar materias por malla
    
    * refactoring paginación, ordenamiento y busqueda
    
    * Paginacion

--- app/Repositories/MatterMeshRepository.php
+++ app/Repositories/MatterMeshRepository.php
@@ -97,6 +97,6 @@ class MatterMeshRepository extends BaseRepository
         $sort = isset(request()->query()['sort']) ? request()->query()['sort'] : 'id';
         $type_sort = isset(request()->query()['type_sort']) ? request()->query()['type_sort'] : 'desc';
 
-        return $query->orderBy($sort, $type_sort)->paginate(isset(request()->query()['size']) ?: 100)->where("mesh_id",$id);
+        return $query->where("mesh_id",$id)->orderBy($sort, $type_sort)->paginate(isset(request()->query()['size']) ?: 100);
     }
 }
