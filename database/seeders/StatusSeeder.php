ff3cb4e4f80f35ba372cf5d775fa928b0815af8a
jorgediazmontoya <87205178+jorgediazmontoya@users.noreply.github.com>
2021-09-29 14:46:34 -0500

    Se agregado estados de mallas (#206)

--- database/seeders/StatusSeeder.php
+++ database/seeders/StatusSeeder.php
@@ -14,6 +14,7 @@ class StatusSeeder extends Seeder
      */
     public function run()
     {
+        // No cambiar el orden de los estado. Si se tiene que agregar nuevos estados, se los ubicará al final.
         DB::connection('tenant')->table('status')->insert([
             ['st_name' => 'Activo', 'category_status_id' => '1'],
             ['st_name' => 'Inactivo', 'category_status_id' => '1'],
@@ -21,8 +22,9 @@ class StatusSeeder extends Seeder
             ['st_name' => 'Ingreso', 'category_status_id' => '4'],
             ['st_name' => 'Vigente', 'category_status_id' => '4'],
             ['st_name' => 'No vigente', 'category_status_id' => '4'],
-            ['st_name' => 'Publicado', 'category_status_id' => '5'],
-            ['st_name' => 'No Publicado', 'category_status_id' => '5'],
+            ['st_name' => 'Vigente', 'category_status_id' => '5'],
+            ['st_name' => 'No vigente', 'category_status_id' => '5'],
+            ['st_name' => 'En espera', 'category_status_id' => '5'],
         ]);
     }
 }
