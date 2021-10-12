5575a914457206e3e0afe5cfa3111a5859f8585b
jorgediazmontoya <87205178+jorgediazmontoya@users.noreply.github.com>
2021-10-12 16:30:44 -0500

    Se agrgeo el correo al seeder de user_profile (#298)

--- database/seeders/UserProfileSeeder.php
+++ database/seeders/UserProfileSeeder.php
@@ -15,9 +15,9 @@ class UserProfileSeeder extends Seeder
     public function run()
     {
         DB::connection('tenant')->table('user_profiles')->insert([
-            ['user_id' => 1, 'profile_id' => 3, 'status_id' => 1],
-            ['user_id' => 2, 'profile_id' => 3, 'status_id' => 1],
-            ['user_id' => 2, 'profile_id' => 4, 'status_id' => 1]
+            ['user_id' => 1, 'profile_id' => 3, 'email' => 'jsanchez@redlinks.com.ec', 'status_id' => 1],
+            ['user_id' => 2, 'profile_id' => 3, 'email' => 'jdiaz@redlinks.com.ec', 'status_id' => 1],
+            ['user_id' => 2, 'profile_id' => 4, 'email' => 'clata@redlinks.com.ec', 'status_id' => 1]
         ]);
     }
 }
